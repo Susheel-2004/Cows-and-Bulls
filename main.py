0dd7e0b979a0f3d9e1bd51a0108e3aded98f4225
Susheel-2004 <68869282+Susheel-2004@users.noreply.github.com>
2020-09-26 13:02:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
 import random
 
+print("COWS AND BULLS: Single Player")
+print("""Instructions:
+You are guessing the 4-Digit number in my mind
+No repetitions
+"B" represents the number you guessed consists digit(s) which are in the right position
+"C" represents the number you guessed consists digits that are correct but not in their exact position
+Use your deductive skills to chase down my number
+All the best!!""")
+
+
+def game():
+
+    k = 0
+    while True:
+        k += 1
+        guess = str(input(f"Guess{k}: "))
+        cows = 0
+        bulls = 0
+        if len(guess) != 4:
+            print("Invalid Input")
+        else:
+            for i in range(4):
+                if guess[i] == (first+last)[i]:
+                    bulls += 1
+                elif guess[i] in first+last:
+                    cows += 1
+            print(f"{bulls}B {cows}C")
+            if bulls == 4:
+                print("You won!!")
+                print(f"You took {k} guess(es)")
+                break
+
+
+def replay():
+
+    re = str(input("Do you want to play again(yes/no)? ")).lower()
+    if re == "yes":
+        game()
+    else:
+        exit(1)
+
 
 digits = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 random.shuffle(digits)
@@ -7,24 +48,6 @@ first = [digits[0]]
 digits[0] = "0"
 random.shuffle(digits)
 last = digits[0:3]
-
-k = 0
-i = 0
-while True:
-    k += 1
-    guess = str(input(f"Guess{k}: "))
-    cows = 0
-    bulls = 0
-    if len(guess) != 4:
-        print("Invalid Input")
-    else:
-        for i in range(4):
-            if guess[i] == (first+last)[i]:
-                bulls += 1
-            elif guess[i] in first+last:
-                cows += 1
-        print(f"{bulls}B {cows}C")
-        if bulls == 4:
-            print("You won!!")
-            print(f"You took {k} guess(es)")
-            break
+print(first+last)
+game()
+replay()
